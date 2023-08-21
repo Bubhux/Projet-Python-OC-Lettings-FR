a6f04504fbc6517c8f013860a87c6d509f171bea
Bubhux <bubhuxpaindepice@gmail.com>
2023-08-21 18:55:21 +0200

    Suppression du dossier venv sur le référentiel GitHub

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\bubhux\desktop\op projet 13\orange-county-lettings\python-oc-lettings-fr\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
