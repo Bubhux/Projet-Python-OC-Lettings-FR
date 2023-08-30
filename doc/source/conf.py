7cdde519efa3425230c4d1ce22b799d5b8c34800
Bubhux <bubhuxpaindepice@gmail.com>
2023-08-31 01:25:15 +0200

    Mise à jour ajout de Read the Docs Sphinx

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -6,15 +6,18 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'doc-oc-lettings-apps'
+project = 'oc-lettings'
 copyright = '2023, Jonathan.P'
 author = 'Jonathan.P'
-release = '1'
+release = 'v.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx_rtd_theme',
+]
+
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +27,10 @@ language = 'fr'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+html_theme_options = {
+    "sidebarbgcolor": "grey",
+    "bgcolor": "white"
+}
---
+++ doc/source/guide-utilisation.rst
@@ -0,0 +1,4 @@
+=======
+Titre
+=======
+Contenu du document...
